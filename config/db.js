e1a86f7cbc58d2fd95829785a5cac4fd9c650347
Akash Dhankar <akashdhankar.2023it@sece.ac.in>
2025-01-30 21:49:52 +0530

    save

--- config/db.js
+++ config/db.js
@@ -1,43 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
-const dotenv = require("dotenv");
 
-
-dotenv.config();
-
-const { mongoURI } = require("./config");
-const authRoutes = require("./routes/Auth");
-const projectRoutes = require("./routes/Projects");
+// Import routes
+const authRoutes = require("./routes/auth");
+const projectRoutes = require("./routes/projects");
 const taskRoutes = require("./routes/tasks");
 
+// Configurations
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;  
+const PORT = process.env.PORT || 5000;
 
-
-app.use(cors({
-    // origin: 'http://localhost:5175',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// Middleware
+app.use(cors());
 app.use(express.json());
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 
-
+// Serve frontend
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-
+// Connect to MongoDB and start server
 mongoose
-  .connect(mongoURI) 
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
